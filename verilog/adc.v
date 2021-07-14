444cb4da7800719e7fc1e7d59c6e176fc145d4b3
Mohamed Shalan <mshalan@aucegypt.edu>
2021-07-14 12:45:46 +0200

    Indicating the type of each port

--- verilog/adc.v
+++ verilog/adc.v
@@ -1,19 +1,19 @@
 module adc(
-    input   wire        clk,    // The clock (digital)
-    input   wire        rstn,   // Active low reset (digital)
-    input   wire        start,  // Conversion start (digital)
-    output  wire        done,   // Conversion is done (digital)
-    output  wire [7:0]  data,   // SAR o/p (digital)
-    input   wire        cmp_sel,// 0: Internal Comparator, 1: External Comparator
-    input   wire        cmp,
+    input   wire        clk,        // The clock (digital)
+    input   wire        rstn,       // Active low reset (digital)
+    input   wire        start,      // Conversion start (digital)
+    output  wire        done,       // Conversion is done (digital)
+    output  wire [7:0]  data,       // SAR o/p (digital)
+    input   wire        cmp_sel,    // 0: Internal Comparator, 1: External Comparator (digital)
+    input   wire        cmp,        // external comparison  (digital)
 
-    // ACMP Ports for debugging
-    input   wire        INP,    // (Analog)
-    input   wire        INN,    // (Analog)
-    output  wire        Q,       // (digital)
+    // ACMP Ports 
+    input   wire        INP,        // (Analog)
+    input   wire        INN,        // (Analog)
+    output  wire        Q,          // (digital)
 
-    // DAC ports
-    input   wire        d0,
+    // DAC Ports    
+    input   wire        d0,         // DAC input data (digital)
     input   wire        d1,
     input   wire        d2,
     input   wire        d3,
@@ -21,8 +21,8 @@ module adc(
     input   wire        d5,
     input   wire        d6,
     input   wire        d7,
-    input   wire        dac_inp1,
-    output  wire        dac_out_v
+    input   wire        dac_inp1,   // (Analog)
+    output  wire        dac_out_v   // (Analog)
 
 );
 
