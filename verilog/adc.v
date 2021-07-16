eb709547a7971701c70ccb808db44b86a23e8ddc
Mohamed Shalan <mshalan@aucegypt.edu>
2021-07-16 23:00:44 +0200

    Added HVL Comparator and updated the adc module

--- verilog/adc.v
+++ verilog/adc.v
@@ -1,27 +1,22 @@
 module adc(
-`ifdef USE_POWER_PINS
-    input wire vccd1,	        // User area 1 1.8V supply
-    input wire vssd1,	        // User area 1 digital ground
-    input wire vccd2,	        // User area 2 1.8V supply (analog)
-    input wire vssd2,	        // User area 2 ground      (analog)
-`endif
     input   wire        clk,        // The clock (digital)
-    input   wire        rst,       // Active low reset (digital)
+    input   wire        rstn,       // Active low reset (digital)
     input   wire        start,      // Conversion start (digital)
     output  wire        done,       // Conversion is done (digital)
     output  wire [7:0]  data,       // SAR o/p (digital)
     input   wire        cmp_sel,    // 0: Internal Comparator, 1: External Comparator (digital)
     input   wire        cmp,        // external comparison  (digital)
 
-    // ACMP (HD) Ports 
+    // ACMP Ports 
     input   wire        INP,        // (Analog)
     input   wire        INN,        // (Analog)
     output  wire        Q,          // (digital)
 
-    // Secondary ACMP (HVL) Ports 
+    // Secondry ACMP
     input   wire        sINP,        // (Analog)
     input   wire        sINN,        // (Analog)
     output  wire        sQ,          // (digital)
+    
 
     // DAC Ports    
     input   wire        d0,         // DAC input data (digital)
@@ -41,21 +36,7 @@ module adc(
     wire        clkn;
     wire        sar_cmp = cmp_sel ? cmp : Q;
     
-    ACMP_HVL SCOMP (
-        .clk(clk),
-        .INP(sINP),
-        .INN(sINN),
-        .Q(sQ)
-    );
-
-    
     ACMP COMP (
-    `ifdef USE_POWER_PINS
-        .vccd2(vccd2),
-        .vssd2(vssd2),
-        .VDD(vccd2),
-        .VSS(vssd2),
-    `endif
         .clk(clk),
         .INP(INP),
         .INN(INN),
@@ -64,7 +45,7 @@ module adc(
 
     SAR CTRL ( 
         .clk(clk),   
-        .rst(rst),  
+        .rstn(rstn),  
         .start(start),
         .cmp(sar_cmp),
         .out(data),    
@@ -72,12 +53,9 @@ module adc(
         .done(done),
         .clkn(clkn) 
     );
+	
 
-    DAC_8BIT DAC (
-     `ifdef USE_POWER_PINS
-        .vdd(vccd2),
-        .gnd(vssd2),
-    `endif 
+    dac_8bit DAC (
         .d0(d0),
         .d1(d1),
         .d2(d2),
@@ -86,9 +64,11 @@ module adc(
         .d5(d5),
         .d6(d6),
         .d7(d7),
+        
         .inp1(dac_inp1),
+        
         .out_v(dac_out_v)
-    );
+);
 
 
 endmodule
